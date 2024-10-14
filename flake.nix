c930c1d91b769257759b401ff2c9d920a773b8ca
Sridhar Ratnakumar <srid@srid.ca>
2024-10-13 23:00:20 -0400

    Initialize next.js project

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A devShell with nodejs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+          ];
+        };
+      };
+    };
+}
