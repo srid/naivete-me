153f0cd69ab92a32c93ed8c9306c47f547089203
Sridhar Ratnakumar <srid@srid.ca>
2024-10-13 22:54:33 -0400

    edits, r

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -65,13 +65,13 @@ fn NaivelyOpenAndSincere() -> impl IntoView {
         <h3 id="open">"Naively open and sincere"</h3>
 
         <p>
-            "The usual modus operandi when investigating feelings is to be closed (and cunning), by predicting a particular way of doing it and framing oneself in that closed fashion. Since this is the only moment there is — and the future cannot be predestined — one might as well remain open (be naively open) to whatever pops up. Stay fascinatedly curious as to how this moment will unfold: whenever one is no longer feeling good, one can look at "
+            "The usual modus operandi in approaching life more generally, and when investigating feelings in particular, is to be closed (even cunning), by predicting a particular way of doing it and framing oneself in that closed fashion. Since this is the only moment there is — and the future cannot be predestined — one might as well remain open (be naively open) to whatever pops up all the while being totally receptive and appreciative of what is happening. Stay fascinatedly curious as to how this moment will unfold: whenever one is no longer feeling good, one can look at "
             <strong>"whatever"</strong>
             " that is, without needing to maintain a premonition about it."
         </p>
 
         <p>
-            The openness of naiveté acts to counteract the self-centered outlook of the identity.
+            "The openness of naiveté acts to counteract the self-centered outlook of the identity which also normally wants to be in control."
         </p>
     }
 }
